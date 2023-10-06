e162fbc7520575020a9018cb06dcb868a156eedc
limetree <limetree411@gmail.com>
2023-10-06 15:24:21 +0900

    mouse test

--- keyboards/splitkb/aurora/lily58/keymaps/limetree/keymap.c
+++ keyboards/splitkb/aurora/lily58/keymaps/limetree/keymap.c
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_BTN1,  KC_BTN2,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
                       KC_LGUI, KC_LALT, MO(_LOWER), KC_SPC,        KC_ENT, MO(_RAISE), KC_BSPC, KC_LALT
 ),
     
@@ -144,15 +144,17 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     // 0 is left-half encoder
     // 1 is right-half encoder
     if (index == 0) {
-        tap_code(KC_0);
+        if (clockwise) {
+        tap_code16(KC_MS_RIGHT);
+      } else {
+        tap_code(KC_MS_LEFT);
+      }
     } else if (index == 1) {
-        tap_code(KC_1);
-    }
-
-    if (clockwise) {
-        tap_code16(KC_PLUS);
-    } else {
-        tap_code(KC_MINUS);
+        if (clockwise) {
+        tap_code16(KC_MS_UP);
+      } else {
+        tap_code(KC_MS_DOWN);
+      }
     }
 
     return false;
